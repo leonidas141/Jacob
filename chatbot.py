cd5121f3383c5846463b37fe82314e899e2aec4e
wangsiqiang <wangsiqiang@163.com>
2021-03-18 02:51:59 +0800

    first usable

--- chatbot.py
+++ chatbot.py
@@ -1,66 +1,15 @@
 import os
-import random
-import json
-import pickle
-import numpy as np
+from pa import GenericAssistant
 
-import nltk
-# nltk.download('punkt')
-# nltk.download('wordnet')
-from nltk.stem import WordNetLemmatizer
+pa = GenericAssistant(os.path.join("data","intents.json"), model_name="test_model")
+pa.train_model()
+pa.save_model()
 
-from tensorflow.keras.models import load_model
+done = False
 
-lemmatizer = WordNetLemmatizer()
-
-intents = json.loads(open(os.path.join("data","intents.json")).read())
-
-words = pickle.load(open("words.pkl","rb"))
-classes = pickle.load(open("classes.pkl","rb"))
-
-model = load_model("chatbotv01.h5")
-
-def clean_up_sentence(sentence):
-    sentence_words = nltk.word_tokenize(sentence)
-    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
-    return sentence_words
-
-def bag_of_words(sentence):
-    sentence_words = clean_up_sentence(sentence)
-    bag = [0] * len(words)
-    for w in sentence_words:
-        for i, word in enumerate(words):
-            if word == w:
-                bag[i] = 1
-    return np.array(bag)
-
-def predict_class(sentence):
-    bow = bag_of_words(sentence)
-    res = model.predict(np.array(bow))[0]
-    ERROR_TH = 0.25
-    result = [[i, r] for i, r in enumerate(res) if r > ERROR_TH]
-
-    result.sort(key=lambda x: x[1], reverse=True)
-    return_list = []
-    for r in results:
-        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
-    return return_list
-
-def get_response(intent_list, intent_json):
-    tag = intent_list[0]["intent"]
-    list_of_intents = intent_json["intents"]
-    for i in list_of_intents:
-        if i["tag"] == tag:
-            result = random.choice(i["responses"])
-            break
-    return result
-
-print("GO, bot is running")
-
-while True:
-    message = input("")
-    ints = predict_class(message)
-    res = get_response(ints, intents)
-    print(res)
-
-
+while not done:
+    message = input("Enter a message: ")
+    if message == "STOP":
+        done = True
+    else:
+        pa.request(message)
